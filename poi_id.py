3a8b0bb3fa1a58a5e547022c36d06acd697f6928
Tim Mac <timwlindsey@gmail.com>
2016-05-23 22:03:11 -0500

    Completed Commit 1

--- poi_id.py
+++ poi_id.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import sys
 import pickle
 sys.path.append("../tools/")
@@ -10,14 +9,27 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','total_payments', 'from_this_person_to_poi', 'total_stock_value', 'expenses', 'long_term_incentive'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-### Task 3: Create new feature(s)
+#Kenneth Lay and the bogus "TOTAL" parameter both are outside acceptable range, deleting as outliers
+del data_dict['TOTAL']
+del data_dict['LAY KENNETH L']
+
+#Track all names that don't have payments info
+to_remove = []
+for key in data_dict:
+	if data_dict[key]['total_payments'] == 'NaN':
+		to_remove.append(key)
+
+#delete anyone without payment info
+for name in to_remove:
+	del data_dict[name]
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -32,8 +44,8 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(max_features=5)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -45,8 +57,13 @@ clf = GaussianNB()
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+    train_test_split(features, labels)
 
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
